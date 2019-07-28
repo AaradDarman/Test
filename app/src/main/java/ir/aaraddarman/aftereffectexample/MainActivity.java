be2bd3c359bf77de7f04e5e1977bd8075b1ef608
AaradDarman <aaraddarman@gmail.com>
2019-07-28 18:35:55 +0430

    Test Commit

--- app/src/main/java/ir/aaraddarman/aftereffectexample/MainActivity.java
+++ app/src/main/java/ir/aaraddarman/aftereffectexample/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Log.i("test","test");
+    Log.i("test","tests");
   }
 }
